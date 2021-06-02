75b0a0926b59e9392834f4f85b28bff1074e3eb4
kaylainsyira <83621348+kaylainsyira@users.noreply.github.com>
2021-06-03 00:20:44 +0700

    Update TextSimpleKerning.js

--- TextSimpleKerning/TextSimpleKerning.js
+++ TextSimpleKerning/TextSimpleKerning.js
@@ -1,37 +1,112 @@
-/*
-A simple sketch about Letterspace
-by k.donnachie for CodeWords, 2020
-Can be also be mapped to mouse position or scroll... or... (rather than the slider)
-Can also be used for writing text letter by letter, 
-by introducing a frameCount check before the text() line (29 in this original)
-uses p5js textWidth() 
-*/
+var Juliet;
+var Supply;
+let sentence = ['Love is', 'heavy & light', 'bright & dark', 'asleep & awake', 'Its everything except what it is!', 'For never', 'was a story of', 'more woe', 'than this of', 'JULIET AND HER ROMEO'];
+let word;
+let ts = 200;
+let speed = 5;
+let song;
 
-var f, kerning, x, slider, font;
-var message = [];
+
 function preload() {
-  font = loadFont('data/HelveticaNowDisplay.otf');   // this file must exist
+  Juliet = loadFont('data/Juliet-Serif.otf');
+  Supply = loadFont('data/Supply-Regular.otf');
+  song = loadSound('data/SOUND-RJ.mp3');
 }
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  textFont(font,20,true);
-  kerning=1.6;
-  message='To Each character it\'s own space.';
-  x=20;
-  slider = createSlider(0.0, 3.0, 0.1, 0);
-  slider.position(15, 10);
-  slider.style('width', '100px');
+  textFont(Juliet);
+  textAlign(CENTER, CENTER);
+  word = sentence[0];
+
+
+  setTimeout(one, 2000);
+  setTimeout(two, 3000);
+  setTimeout(three, 4000);
+  setTimeout(four, 5000);
+  setTimeout(five, 8500);
+  setTimeout(six, 10000);
+  setTimeout(seven, 14000);
+  setTimeout(eight, 16000);
+  setTimeout(nine, 18000);
 }
-function draw() { 
-  background(255);
+
+function one(){
+  word = sentence[1];
+  ts = 100;
+  speed = 5;
+}
+
+function two(){
+  word = sentence[2];
+  ts = 100;
+  speed = 5;
+}
+
+function three(){
+  word = sentence[3];
+  ts = 100;
+  speed = 5;
+}
+
+function four(){
+  word = sentence[4];
+  ts = 100;
+  speed = 5;
+}
+
+function five(){
+  word = sentence[5];
+  ts = 150;
+  speed = 5;
+}
+
+function six(){
+  word = sentence[6];
+  ts = 100;
+  speed = 5;
+}
+
+function seven(){
+  word = sentence[7];
+  ts = 150;
+  speed = 5;
+}
+
+function eight(){
+  word = sentence[8];
+  ts = 100;
+  speed = 5;
+}
+
+function nine(){
+  word = sentence[9];
+  ts = 150;
+  speed = 5;
+}
+
+function draw() {
+  background(196, 58, 35);
+  textFont(Juliet);
+  textSize(ts);
+  text(word, 0.15*width, 0.15*height, 0.7*width, 0.7*height);
+  
+  song.setVolume(map(mouseX, 0, width, 0, 1));
+  
   fill(0);
-  kerning = slider.value();
-  x = 20;
-  textSize(14);
-  text('Letterspace: '+ nf(kerning, 1, 2), 20, 50);
-  for (var i = 0; i < message.length; i++) {
-    textSize(24);
-    text(message[i],x,height/2);
-    x += int(textWidth(message[i])*kerning); 
-  }
+  noStroke();
+  textFont(Supply);
+  textSize(9);
+  text('Juliet Serif by Kayla Insyira (2021)', width/2, 0.925*height);
+  
+  fill(0);
+  noStroke();
+  textFont(Supply);
+  textSize(9);
+  text('click anywhere for sound, move for volume', width/2, 0.075*height);
+  
+}
+
+function mousePressed() {
+  song.play();
 }
